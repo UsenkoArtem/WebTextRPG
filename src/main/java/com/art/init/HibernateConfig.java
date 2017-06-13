e80c2e00039065465e422f0ad2eeaf8f17e45304
Artem Usenko <usenko.job@gmail.com>
2017-06-13 14:44:06 +0300

    Bug fix

--- src/main/java/com/art/init/HibernateConfig.java
+++ src/main/java/com/art/init/HibernateConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -21,57 +21,64 @@ import java.util.Properties;
 public class HibernateConfig {
 
     @Bean(name = "Session")
-    public LocalSessionFactoryBean SessionFactory (){
+    public LocalSessionFactoryBean SessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("com.art");
         sessionFactory.setHibernateProperties(hibernateProperties());
-        return  sessionFactory;
+        return sessionFactory;
     }
 
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect","org.hibernate.dialect.MySQL5InnoDBDialect");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
         properties.put("hibernate.show_sql", "true");
-        properties.put("hibernate.format_sql","true");
-        properties.put("hibernate.connection.autocommit","true");
-        properties.put("connection.autoReconnect=true","true");
-        properties.put("hibernate.autoReconnect","true");
-        properties.put("hibernate.connection.pool_size=","10");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.connection.autocommit", "true");
+        properties.put("connection.autoReconnect=true", "true");
+        properties.put("hibernate.autoReconnect", "true");
+        properties.put("hibernate.connection.pool_size=", "10");
+        properties.put("checkoutTimeout", "30000");
+        properties.put("idleConnectionTestPeriod", "10");
+        properties.put("initialPoolSize", "10");
+        properties.put("maxIdleTime", "30");
+        properties.put("maxPoolSize", "100");
+        properties.put("minPoolSize", "10");
+        properties.put("maxStatements", "10");
         return properties;
     }
-/**
+
+    /**
+     * @Bean public DataSource dataSource() {
+     * DriverManagerDataSource dataSourse = new DriverManagerDataSource();
+     * dataSourse.setDriverClassName(environment.getRequiredProperty("jdbc.driverClass"));
+     * dataSourse.setUsername(environment.getRequiredProperty("jdbc.user"));
+     * dataSourse.setPassword(environment.getRequiredProperty("jdbc.pass"));
+     * dataSourse.setUrl(environment.getRequiredProperty("jdbc.url"));
+     * return dataSourse;
+     * }
+     */
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSourse = new DriverManagerDataSource();
-        dataSourse.setDriverClassName(environment.getRequiredProperty("jdbc.driverClass"));
-        dataSourse.setUsername(environment.getRequiredProperty("jdbc.user"));
-        dataSourse.setPassword(environment.getRequiredProperty("jdbc.pass"));
-        dataSourse.setUrl(environment.getRequiredProperty("jdbc.url"));
-    return dataSourse;
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
-*/
-@Bean
-public DataSource dataSource() {
-    String dbUrl = System.getenv("JDBC_DATABASE_URL");
-    String username = System.getenv("JDBC_DATABASE_USERNAME");
-    String password = System.getenv("JDBC_DATABASE_PASSWORD");
-
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-    dataSource.setUrl(dbUrl);
-    dataSource.setUsername(username);
-    dataSource.setPassword(password);
-    return dataSource;
-}
 
 
     @Bean
     @Autowired
-    public HibernateTransactionManager transactionManager(SessionFactory s ) {
+    public HibernateTransactionManager transactionManager(SessionFactory s) {
         HibernateTransactionManager txManager = new HibernateTransactionManager();
         txManager.setSessionFactory(s);
-        return txManager ;
+        return txManager;
     }
 }
